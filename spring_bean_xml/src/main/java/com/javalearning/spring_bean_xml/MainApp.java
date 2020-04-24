f6184496725e49046e7813cdfc2f58ed57fe9b5f
sms.cole <sms.cole@cole-lap.net.fpt>
2020-04-24 20:08:11 +0700

    update getBean function

--- spring_bean_xml/src/main/java/com/javalearning/spring_bean_xml/MainApp.java
+++ spring_bean_xml/src/main/java/com/javalearning/spring_bean_xml/MainApp.java
@@ -7,11 +7,11 @@ public class MainApp {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext springContext = new ClassPathXmlApplicationContext("beans.xml");
 		
-		HelloSpringCore helloSpringCore = (HelloSpringCore)springContext.getBean("beanHelloId");
+		HelloSpringCore helloSpringCore = springContext.getBean("beanHelloId", HelloSpringCore.class);
 		String str = helloSpringCore.buildToString();
 		System.out.println(str);
 		
-		HelloSpringCore bean = (HelloSpringCore)springContext.getBean("myBean");
+		HelloSpringCore bean = springContext.getBean("myBean", HelloSpringCore.class);
 		str = bean.buildToString();
 		System.out.println(str);
 		
